e3fa0962e750de59a12c257d2c439acd40dcc752
A Jeselle <aly.yanga.03@gmail.com>
2026-03-17 19:51:07 +0800

    Refactor code structure for improved readability and maintainability

--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url, { family: 4 })
-    .then(result => {
+    .then(() => {
         console.log('connected to MongoDB')
     })
     .catch(error => {
@@ -14,8 +14,22 @@ mongoose.connect(url, { family: 4 })
     })
 
 const phoneBookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: [3, '{VALUE} is shorter than the minimum allowed length (3)'],
+        required: [true, 'Must have a name']
+    },
+    number: {
+        type: String,
+        minLength: [8, '{VALUE} is shorter than the minimum allowed length (8)'],
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'Must have a number']
+    },
 })
 
 phoneBookSchema.set('toJSON', {
